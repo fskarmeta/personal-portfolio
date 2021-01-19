11fbc24ffc9c56005f1157da8cc9fb33094b3ac1
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-01-19 02:58:47 -0300

    google analytics added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Navbar from "./components/navbar";
@@ -6,11 +6,18 @@ import Projects from "./views/projects";
 import Contact from "./views/contact";
 import AboutMe from "./views/about";
 import Certifications from "./views/certifications";
+import ReactGA from "react-ga";
 import "./styles/app.css";
 
 function App() {
   const basename = process.env.BASENAME || "";
   const [inside, setInside] = useState(false);
+
+  useEffect(() => {
+    ReactGA.initialize("2252807390");
+    ReactGA.pageview(window.location.pathname);
+  });
+
   return (
     <Container fluid className="outside">
       <div
