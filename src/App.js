416b89674e9f20d312881a947b3245e4d8329b92
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-02-01 17:24:11 -0300

    test

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ function App() {
                 <Route exact path="/certificates">
                   <Certifications />
                 </Route>
-                <Redirect from="/:any" to="/" />
               </Container>
             </Switch>
           </Analytics>
